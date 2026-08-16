66517394e9ff8ef5a529383f2b08f97c5714ad8a
Deepakbagili05 <237r1a6604@cmrtc.ac.in>
2026-08-16 20:29:47 +0530

    Fix deployment and database connection

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,16 @@
+import os
 import pymysql
-from config import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB", "defaultdb")
+
+connection = None
 
 try:
     connection = pymysql.connect(
@@ -8,6 +19,8 @@ try:
         user=MYSQL_USER,
         password=MYSQL_PASSWORD,
         database=MYSQL_DB,
+        ssl={"ssl": {}},
+        connect_timeout=30,
         cursorclass=pymysql.cursors.DictCursor
     )
 
